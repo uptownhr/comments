89643f74d9b6a37d8a8431ef32ebd26b576a40f9
PeterXay <peterxay@github.com>
2015-05-21 10:58:14 -0700

    [UserRegistration.php]

--- UserRegistration.php
+++ UserRegistration.php
@@ -13,19 +13,17 @@ if ($connect->connect_error)
 
 if(empty($_POST))
 {
- echo "----";
- echo $_POST;
+ echo "";
 }
-else if (!empty($_POST) && $_POST['username'] != "" && $_POST['password'] != "")
+
+else if (!empty($_POST) && $_POST['email'] != "" && $_POST['password'] != "")
 {
- echo "///"; 
+ echo "<b>" . $_POST['email'] . "</b>";
 
- echo "<b>" . $_POST['username'] . "</b>";
-
- $username = $_POST['username'];
+ $email = $_POST['email'];
  $password = $_POST['password'];
 
- $query = "INSERT INTO User (email, password) VALUES ('$username', '$password')";
+ $query = "INSERT INTO User (email, password) VALUES ('$email', '$password')";
  if (mysqli_query($connect, $query)) 
  {
   echo " has been created successfully";
@@ -37,17 +35,17 @@ else if (!empty($_POST) && $_POST['username'] != "" && $_POST['password'] != "")
 
  mysqli_close($conn);
 }
-else if ($_POST['username'] == "" && $_POST['password'] != "")
+else if ($_POST['email'] == "" && $_POST['password'] != "")
 {
- echo "Username field is blank <br>";
+ echo "Email field is blank <br>";
 }
-else if ($_POST['password'] == "" && $_POST['username'] != "")
+else if ($_POST['password'] == "" && $_POST['email'] != "")
 {
  echo "Password field is blank <br>";
 }
-else if ($_POST['username'] == "" && $_POST['password'] == "")
+else if ($_POST['email'] == "" && $_POST['password'] == "")
 {
- echo "Username and Password field are blank <br>";
+ echo "Email and Password field are blank <br>";
 }
 ?>
 
@@ -56,8 +54,8 @@ else if ($_POST['username'] == "" && $_POST['password'] == "")
 <legend>Creating New User</legend>
 <input type='hidden' name='submitted' id='submitted' value='1'/>
  
-<label for='username' >Email:</label>
-<input type='text' name='username' id='username'  maxlength="50" />
+<label for='email' >Email:</label>
+<input type='text' name='email' id='email'  maxlength="50" />
  
 <label for='password' >Password:</label>
 <input type='password' name='password' id='password' maxlength="50" />

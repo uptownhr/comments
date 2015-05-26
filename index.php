1c7e4a71e72bc3d1db828f160ffe94f04fe82e25
James Lee <uptownhr@gmail.com>
2015-05-26 10:23:20 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php
+session_start();
+$logged_in = 'not logged in';
+$email = false;
 
-phpinfo();
+if( !empty($_SESSION['user']) ){
+  $logged_in = 'logged in';
+  $email = $_SESSION['user']['email'];
+}
+?>
+<html>
+  <body>
+    <nav>
+      <ul>
+        <li>Login</li>
+      </ul>
+    </nav>
+    <section>
+      <p>User is <?php echo $logged_in; ?></p>
+      <p>Email is <?php echo $email; ?></p>
+    </section>
+  </body>
+</html>

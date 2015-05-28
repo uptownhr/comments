d169ed70a5136e40692e93bdf3ffc4e8faaac373
PeterXay <peterxay@github.com>
2015-05-28 06:27:36 -0700

    ticket #20, added created table sql command to db.sql

--- db.sql
+++ db.sql
@@ -5,3 +5,20 @@ CREATE TABLE `User` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE 'Site' (
+  'id' int(11) unsigned NOT NULL,
+  'domain' varchar(128) NOT NULL DEFAULT '',
+  'created' datetime default current_timestamp,
+  PRIMARY KEY ('id')
+)
+
+CREATE TABLE 'Comments' (
+  'comment_id' int(11) unsigned NOT NILL,
+  'site_id' int(11) NOT NULL DEFAULT '',
+  'site_unique_id' int(11) NOT NULL DEFAULT '',
+  'user_id' int(11) NOT NULL DEFAULT '',
+  'message' text,
+  'created' datetime, 
+  PRIMARY KEY ('comment_id')
+)
